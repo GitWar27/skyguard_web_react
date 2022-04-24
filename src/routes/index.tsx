abb7f44ed10d8c9592603c459013d21a883a142a
valspng <valsapongay@gmail.com>
2022-04-24 14:28:31 +0800

    add guides

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,21 @@
 import React, { Suspense, lazy } from "react";
 import { Switch, Route } from "react-router-dom";
 
+//**Routes import Guide */
+// just import the index/Main file of the component/view
+//e.g (../views/home) (../pages/contact)
+
 const Home = lazy(() => import("../views/home"));
 const About = lazy(() => import("../pages/about/About"));
 const Careers = lazy(() => import("../pages/career/Career"));
 const Clients = lazy(() => import("../pages/clients/Clients"));
 const Security = lazy(() => import("../components/content/security"));
+
+//** Routes guide */
+// id ---> id of the routes anything is acceptable
+// path ---> exact path/url of the page
+// component ---> imported view/component to be viewed in the SPA
+// exact ---> flag if the url path is exact as the path key. if false any character after the path key will be accepted
 
 const routes: any = [
   {
