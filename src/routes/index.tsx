035d824c58321fd1a1eb4c7195959de4fc3dc916
valspng <valsapongay@gmail.com>
2022-04-24 14:21:48 +0800

    fix routes and downgrade to react 17

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,28 +1,57 @@
-import React, { Suspense, lazy } from 'react';
-import { Routes, Route } from "react-router-dom";
+import React, { Suspense, lazy } from "react";
+import { Switch, Route } from "react-router-dom";
 
-const Home = lazy(() => import('../components/content/security'));
+const Home = lazy(() => import("../views/home"));
+const About = lazy(() => import("../pages/about/About"));
+const Careers = lazy(() => import("../pages/career/Career"));
+const Clients = lazy(() => import("../pages/clients/Clients"));
+const Security = lazy(() => import("../components/content/security"));
 
-const routes:any = [{
-    id: '/',
-    path: '/',
+const routes: any = [
+  {
+    id: "/",
+    path: "/",
     component: Home,
     exact: true,
-  }]
+  },
+  {
+    id: "/about",
+    path: "/about",
+    component: About,
+    exact: true,
+  },
+  {
+    id: "/career",
+    path: "/career",
+    component: Careers,
+    exact: true,
+  },
+  {
+    id: "/clients",
+    path: "/clients",
+    component: Clients,
+    exact: true,
+  },
+  {
+    id: "/security",
+    path: "/security",
+    component: Security,
+    exact: true,
+  },
+];
 
-const AppRoutes = (props:any) => {
+const AppRoutes = (props: any) => {
+  return (
+    <>
+      <Suspense fallback={null}>
+        <Switch>
+          {routes.map((r: any) => (
+            <Route key={r.id} exact component={r.component} path={r.path} />
+          ))}
+        </Switch>
+      </Suspense>
+    </>
+  );
+};
 
-    return (
-        <>
-            <Suspense fallback={null}>
-                <Routes>
-                    {routes.map((r:any) => (
-                <Route key={r.id}  element={r.component} path={r.path} />
-                ))}
-            </Routes>
-            </Suspense>
-        </>
-    );
-  };
-
-export default AppRoutes
+export default AppRoutes;
